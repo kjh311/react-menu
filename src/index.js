7feb59835c9a7cd2f96253db70259baa3d961577
Kevin <kjh311@gmail.com>
2018-06-11 11:34:28 -0700

    firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import Header from './Header';
 import Footer from './Footer';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+  // Initialize Firebase
+  var config = {
+
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Header />, document.getElementById('header'));
