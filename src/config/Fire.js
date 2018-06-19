b4a40f141e52c2804d121574e2244771f493bad7
Kevin <kjh311@gmail.com>
2018-06-19 15:29:10 -0700

    Hide fire

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyDxENJb8ZRshjRqFsdcHo-2T83C-1EYg8I",
-    authDomain: "menu-e94f8.firebaseapp.com",
-    databaseURL: "https://menu-e94f8.firebaseio.com",
-    projectId: "menu-e94f8",
-    storageBucket: "menu-e94f8.appspot.com",
-    messagingSenderId: "691164886655"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
   };
 const fire = firebase.initializeApp(config);
 export default fire;
